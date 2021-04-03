00889075ed2803813f5d91cb17744a3295cba889
ZLHIS1034150 <zlhis1034150@yandex.com>
2021-04-04 01:13:58 +0800

    HHTT

--- 引用文件注册.bat
+++ 引用文件注册.bat
@@ -3,3 +3,4 @@ copy .\1034170\第三方控件\olelib.tlb c:\Windows\System32 /Y
 copy .\1034170\第三方控件\ISHF_Ex.tlb c:\Windows\System32 /Y
 copy .\1034170\第三方控件\SHLEXT.tlb c:\Windows\System32 /Y
 for %%c in (.\1034170\第三方控件\*.ocx) do regsvr32.exe /s %%c 
+.\1034170\第三方控件\c1regsvr.exe .\1034170\第三方控件\olch2x8.ocx -s
